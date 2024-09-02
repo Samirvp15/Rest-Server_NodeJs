27b6ac07a54cb3b4a096061631772f408604b12f
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-09-02 17:22:54 -0500

    update package express

--- database/config.js
+++ database/config.js
@@ -6,7 +6,7 @@ const dbConnection = async() => {
 
     try {
 
-        await mongoose.connect( process.env.MONGODB_CNN);
+        await mongoose.connect(process.env.MONGODB_CNN);
     
         console.log('Base de datos online');
 
